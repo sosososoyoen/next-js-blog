e8343825828d3b60461b4008f8bf8c7ab94e9270
soonmac <hothemp35@gmail.com>
2025-03-04 00:41:51 +0900

    다크모드 적용, 조회수 wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
-import './global.css'
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import { Navbar } from './components/nav'
-import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
-import Footer from './components/footer'
-import { baseUrl } from './sitemap'
+import './global.css';
+import type { Metadata } from 'next';
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
+import { Navbar } from './components/nav';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import Footer from './components/footer';
+import { baseUrl } from './sitemap';
+import { themeEffect } from 'utils/themeEffect';
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
@@ -34,14 +35,14 @@ export const metadata: Metadata = {
       'max-snippet': -1,
     },
   },
-}
+};
 
-const cx = (...classes) => classes.filter(Boolean).join(' ')
+const cx = (...classes) => classes.filter(Boolean).join(' ');
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html
@@ -52,6 +53,11 @@ export default function RootLayout({
         GeistMono.variable
       )}
     >
+      <head>
+        <script
+          dangerouslySetInnerHTML={{ __html: `(${themeEffect.toString()})()` }}
+        />
+      </head>
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
@@ -62,5 +68,5 @@ export default function RootLayout({
         </main>
       </body>
     </html>
-  )
+  );
 }
