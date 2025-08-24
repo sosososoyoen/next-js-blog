98e0c3cdd018dc749f9ff0595ad6169ee18051a8
soonmac <hothemp35@gmail.com>
2025-08-24 21:37:41 +0900

    feat: 필요없는 폰트 삭제

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './global.css';
 import type { Metadata } from 'next';
 import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
-import { Inter, JetBrains_Mono, Rubik_Bubbles } from 'next/font/google';
+import { Rubik_Bubbles } from 'next/font/google';
 import { Navbar } from './components/nav';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
@@ -11,18 +11,6 @@ import { baseUrl } from './sitemap';
 import { themeEffect } from 'utils/themeEffect';
 
 // Google Fonts 설정
-const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-inter',
-});
-
-const jetbrainsMono = JetBrains_Mono({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-jetbrains-mono',
-});
-
 const rubikBubbles = Rubik_Bubbles({
   subsets: ['latin'],
   weight: '400', // Rubik Bubbles는 400 weight만 지원
@@ -72,8 +60,6 @@ export default function RootLayout({
         'text-black bg-white dark:text-white dark:bg-black',
         GeistSans.variable,
         GeistMono.variable,
-        inter.variable,
-        jetbrainsMono.variable,
         rubikBubbles.variable
       )}
     >
