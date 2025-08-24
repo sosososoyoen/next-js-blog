2fa5e49b0c3e4e4473303439240001ec3c9ef5c2
soonmac <hothemp35@gmail.com>
2025-08-24 21:15:58 +0900

    feat: 폰트 추가 및 홈 화면 wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,29 @@
-import { BlogPosts } from 'app/components/posts'
+import { BlogPosts } from 'app/components/posts';
 
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
-      </h1>
+      <div className="mb-8">
+        <div className="flex align-items">
+          <img
+            src="/images/bubbles_3d_1.png"
+            alt="Profile picture"
+            width={300}
+          />
+        </div>
+      </div>
+      <div>
+        <h1 className="text-3xl font-bubbles tracking-tighter mb-4 text-blue-600">
+          oh Bubbles, my Bubbles🐬🐬🐬
+        </h1>
+      </div>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        I mainly write about web development, but I also share my thoughts on
+        various topics. Enjoy reading! 😊
       </p>
       <div className="my-8">
         <BlogPosts />
       </div>
     </section>
-  )
+  );
 }
